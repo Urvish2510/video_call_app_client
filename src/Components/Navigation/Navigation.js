73c67dfd36f2373054fde22412ef25fe560ec797
Urvish Patel <uvpatel2510@gmail.com>
2023-07-28 19:12:55 +0530

    first commit

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import cuckoologo from '../../Icons/cuckoo-logo.svg'
+import '../Navigation/Navigation.css'
+
+const Navigation = () => {
+    return (
+        <header className="dropShadow">
+            <div className="headerWrapper">
+                <div className="headerContainer flex">
+                    <div className="headerLogoLinkWrapper">
+                        <div className="headerLogoLink">
+                        <a href='/'>
+                            <div className="headerLogo flex flex-row">
+                                <div className="logoImg">
+                                    <img src={cuckoologo} alt="Cuckoo Logo"/>
+                                </div>
+                                <div className="logoText">
+                                    cuckoo
+                                </div>
+                            </div>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+export default Navigation
